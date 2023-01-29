fab8ae2a08f44793e96f36197bfab88a0c8b8eaf
Godavari Bandgar <bandgar 101@gmail.com>
2023-01-29 13:58:57 +0530

    New project

---
+++ src/Division/TwoNoDivision.java
@@ -0,0 +1,15 @@
+package Division;
+
+public class TwoNoDivision {
+	
+	public static void main(String[] args) {
+		
+		int a=50;
+		int b=10;
+		int c=a/b;
+		
+		System.out.println(c);
+		
+	}
+
+}
---
+++ src/addition/TwoNoAddition.java
@@ -0,0 +1,12 @@
+package addition;
+
+public class TwoNoAddition {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
